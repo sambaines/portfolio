a2b09a1929e7ebed39218c732d2f2b19efe24e2c
sambaines <conceptualbaines@gmail.com>
2020-01-27 23:07:39 +0000

    Moved blog posts to appear on homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,32 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`index.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
+<script>
+	export let posts;
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+	
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Sam Baines</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Web Designer</h1>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
+<ul>
+	{#each posts as post}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
+	{/each}
+</ul>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
